12817b989417828043c6f8e69cdf2b77d0bb03f3
StevenShaoYY <635178917@qq.com>
2017-07-14 17:40:17 +0800

    Signed-off-by: StevenShaoYY <635178917@qq.com> 搜索页面

--- src/api/search.js
+++ src/api/search.js
@@ -1,20 +1,20 @@
-import jsonp from 'common/js/jsonp'
-import {commonParams, options} from './config'
+import jsonp from 'common/js/jsonp';
+import { commonParams, options } from './config';
 
-export function getHotKey() {
-  const url = 'https://c.y.qq.com/splcloud/fcgi-bin/gethotkey.fcg'
+export function getHotKey () {
+  const url = 'https://c.y.qq.com/splcloud/fcgi-bin/gethotkey.fcg';
 
   const data = Object.assign({}, commonParams, {
     uin: 0,
     needNewCode: 1,
     platform: 'h5'
-  })
+  });
 
-  return jsonp(url, data, options)
+  return jsonp(url, data, options);
 }
 
-export function search(query, page, zhida, perpage) {
-  const url = 'https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp'
+export function search (query, page, zhida, perpage) {
+  const url = 'https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp';
 
   const data = Object.assign({}, commonParams, {
     w: query,
@@ -32,7 +32,7 @@ export function search(query, page, zhida, perpage) {
     uin: 0,
     needNewCode: 1,
     platform: 'h5'
-  })
+  });
 
-  return jsonp(url, data, options)
+  return jsonp(url, data, options);
 }
